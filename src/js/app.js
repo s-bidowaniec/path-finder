99f7abc2d3bf068cc97f3af50a77c7c908995033
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-05-22 13:23:27 +0200

    Add basic JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,47 @@
-console.log('hello world');
+import { select, classNames } from './settings.js';
+
+const app = {
+  getDomElements:function(){
+    const thisApp =this;
+
+    thisApp.dom = {};
+    thisApp.dom.pages = document.getElementById(select.containerOf.pages).children;
+    thisApp.dom.navlinks = document.querySelectorAll(select.nav.links);
+  },
+  initPages: function() {
+    const thisApp = this;
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+  },
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    /* Add class "active" to matching page, remove from non-matching */
+    for (let page of thisApp.dom.pages) {
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+    /* Add class "active" to matching link, remove from non-matching */
+    for (let link of thisApp.dom.navlinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') === `#${pageId}`
+      );
+    }
+  },
+  init: function() {
+    const thisApp = this;
+
+    thisApp.getDomElements();
+    thisApp.initPages();
+  },
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,30 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    about: '#about',
+    finder: '#pathfinder',
+  },
+  pathfinder: {
+    title: '.title',
+    array: 'table',
+    button: 'button',
+  },
+  nav: {
+    links: 'nav a',
+  },
+};
+
+export const classNames = {
+  pages: {
+    active: 'active',
+  },
+  nav: {
+    active: 'active',
+  },
+  finderArray: {
+    selected: 'selected',
+    start: 'start',
+    end: 'end',
+    optimal: 'optimal',
+  },
+};
